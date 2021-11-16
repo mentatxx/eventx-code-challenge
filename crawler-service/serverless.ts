2af6eeb6104b1eec9518dfb4e4304a735ccd13b7
Alexey Petushkov <mentatxx@gmail.com>
2021-11-16 17:01:47 +0300

    Fixed persisting to S3

--- crawler-service/serverless.ts
+++ crawler-service/serverless.ts
@@ -3,11 +3,13 @@ import crawler from './src/functions/cryptonator-crawler';
 
 const serverlessConfiguration: AWS = {
   service: 'cryptonator-crawler',
+  org: 'mentatxx1',
+  app: 'cryptonator-crawler',
   frameworkVersion: '2',
   plugins: ['serverless-esbuild'],
   provider: {
     name: 'aws',
-    profile: "${file(./config/${opt:stage, 'dev'}.json):profile}",
+    region: 'eu-west-1',
     runtime: 'nodejs14.x',
     apiGateway: {
       minimumCompressionSize: 1024,
@@ -17,13 +19,12 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
       BUCKET_NAME: "${file(./config/${opt:stage, 'dev'}.json):S3Bucket}",
-      S3_REGION: "${file(./config/${opt:stage, 'dev'}.json):S3Region}",
     },
     iamRoleStatements: [
       {
         Effect: 'Allow',
         Action: '*',
-        Resource: "${file(./config/${opt:stage, 'dev'}.json):S3URN}",
+        Resource: "${file(./config/${opt:stage, 'dev'}.json):S3URN}/*",
       },
     ],
     lambdaHashingVersion: '20201221',
--- crawler-service/src/functions/cryptonator-crawler/persist.ts
+++ crawler-service/src/functions/cryptonator-crawler/persist.ts
@@ -1,11 +1,7 @@
-import * as AWS from 'aws-sdk';
 import { TickerByCurrencyPair } from './ticker';
 
-const s3 = new AWS.S3({
-  region: process.env.S3_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
 export async function persistTickers(tickers: TickerByCurrencyPair) {
   await s3
